a5f1644b63674f364597a46ba6ead5bfb2fe77ab
jaganbabusg <sj_babu@yahoo.com>
2026-03-25 15:38:37 +0800

    created and connected sql express

---
+++ Constants/EmailOtpStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace DSO_Email_OTP.Constants
+{
+    public enum EmailOtpStatus
+    {
+        STATUS_EMAIL_OK,
+        STATUS_EMAIL_FAIL,
+        STATUS_EMAIL_INVALID,
+        STATUS_OTP_OK,
+        STATUS_OTP_FAIL,
+        STATUS_OTP_TIMEOUT
+    }
+}
